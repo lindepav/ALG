29856bb87899a69dabc8336d632d3011053b8491
lindepav <lindepav@github.com>
2020-12-31 10:51:29 +0100

    fast, not correct

--- hw08-dynamic_programming/hw08.cpp
+++ hw08-dynamic_programming/hw08.cpp
@@ -6,7 +6,9 @@
 using namespace std;
 using namespace std::chrono; 
 
-void printTable(int **S, int N) 
+int N, C, H;
+
+void printTable(int **S) 
 {
     cout << "--------------" << endl;
     for(int i=0; i<N; i++) {
@@ -18,41 +20,57 @@ void printTable(int **S, int N)
     cout << "--------------" << endl;
 }
 
-int profitOfBuiltPaths(int **acc, int C, int i, int k)
+int getProfit(int **acc, int i, int k) 
 {
-    if(i == k)
-        return 0;
     int best_profit = INT_MIN;
     for(int j=i; j<k; j++) {
         int S1 = acc[i][j];
         int S2 = acc[j+1][k];
         int S_min = min(S1, S2);
         int S_max = max(S1, S2);
-        int S = min(abs(S_min), abs(S_max));
-        int L = k - i;
         int profit = 0;
-        if(S_min < 0 && S_max > 0 && S > L) { // TODO: lze zoptimalizovat
-            profit = S - L;
-        } else {
-            profit = 0;
-        }
+        if(S_min < 0 && S_max > 0) {
+            int S = min(abs(S_min), abs(S_max));
+            int L = k - i;
+            if(S > L)  
+                profit = S - L;
+        } 
         profit -= C;
-        profit += profitOfBuiltPaths(acc, C, i, j) + profitOfBuiltPaths(acc, C, j+1, k);
-        if(profit > best_profit) 
+        int leftProfit = (i != j) ? getProfit(acc, i, j) : 0;
+        int rightProfit = (j+1 != k) ? getProfit(acc, j+1, k) : 0;
+        profit += leftProfit + rightProfit;
+        if(profit > best_profit) {
             best_profit = profit;
+        }
     }
     return best_profit;
 }
 
+int calculateImmediateProfit(int **acc, int i, int j, int k) 
+{
+    int S1 = acc[i][j];
+    int S2 = acc[j+1][k];
+    int S_min = min(S1, S2);
+    int S_max = max(S1, S2);
+    int profit = 0;
+    if(S_min < 0 && S_max > 0) {
+        int S = min(abs(S_min), abs(S_max));
+        int L = k - i;
+        if(S > L)  
+            profit = S - L;
+    } 
+    profit -= C;
+    return profit;
+}
+
 int main(void) 
 {
-    int N, H, C;
     scanf("%d %d %d\n", &N, &H, &C);
     int *P = new int[N];
     for(int i=0; i<N; i++) {
         scanf("%d", &P[i]);
     }
-
+    //cout << "H " << H-1 << endl;
     int **acc = new int*[N];
     for(int i=0; i<N; i++)
         acc[i] = new int[N];
@@ -69,40 +87,56 @@ int main(void)
             acc[i][j] = acc[i][j-1] + acc[j][j];
         }
     }
-    //printTable(S, N);
+    //printTable(acc);
+    H--;
+    int **profits = new int*[N];
+    for(int i=0; i<N; i++) 
+        profits[i] = new int[N];
+    
+    for(int i=0; i<N; i++) {
+        for(int k=0; k<N; k++) {
+            profits[i][k] = 0;
+        }
+    }
+    int i=0, k=1, lvl=1;
+    while(i<N && k<N) {
+        int max_profit = INT_MIN;
+        for(int j=i; j<k; j++) {
+            int current_profit = calculateImmediateProfit(acc, i, j, k);
+            int left_profit = profits[i][j];
+            int right_profit = profits[j+1][k];
+            current_profit += left_profit + right_profit;
+            if(current_profit > max_profit)
+                max_profit = current_profit;
+        }
+        profits[i][k] = max_profit;
+        if(k == N-1) {
+            lvl++; 
+            k = lvl;
+            i = 0;
+        } else {
+            i++; k++;
+        }
+    }
 
-    int bestProfit = 0;
-    for(int j=0; j<N; j++) {
-        for(int i=0; i<=j; i++) {
-            if(H-1 < i) continue;
-            for(int k=j+1; k<N; k++) {
-                if(H-1 > k) continue;
-                int S1 = acc[i][j];
-                int S2 = acc[j+1][k];
-                int S_min = min(S1, S2);
-                int S_max = max(S1, S2);
-                int S = min(abs(S_min), abs(S_max));
-                int L = k - i;
-                int profit = 0;
-                if(S_min < 0 && S_max > 0 && S > L) { // TODO: lze zoptimalizovat
-                    profit = S - L;
-                } else {
-                    profit = 0;
-                }
-                profit -= C;
-                profit += profitOfBuiltPaths(acc, C, i, j) + profitOfBuiltPaths(acc, C, j+1, k);
-
-                if(profit > bestProfit)
-                    bestProfit = profit;
-            }
+    //printTable(profits);
+    int bestProfit = INT_MIN;
+    for(int i=0; i<N; i++) {
+        for(int k=0; k<N; k++) {
+            int profit = profits[i][k];
+            if(profit > bestProfit)
+                bestProfit = profit;
         }
     }
     cout << bestProfit << endl;
 
     delete [] P;
-    for(int i=0; i<N; i++)
+    for(int i=0; i<N; i++) {
         delete[] acc[i];
+        delete[] profits[i];
+    }
     delete [] acc;
+    delete [] profits;
     return 0;
 }
 
